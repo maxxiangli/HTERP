32e49c97da522966f34294e8e83284847a9741d8
macbook <wxt_zhengding@163.com>
2016-11-10 16:35:39 +0800

    1:添加获取联系人管理

---
+++ HTERP/Code/Modules/Contact/model/CHContactListManager.h
@@ -0,0 +1,13 @@
+//
+//  CHContactListManager.h
+//  HTERP
+//
+//  Created by macbook on 10/11/2016.
+//  Copyright © 2016 Max. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CHContactListManager : NSObject
+
+@end
