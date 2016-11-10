f8744018f75020ff6c5215cda78a245d498154cb
macbook <wxt_zhengding@163.com>
2016-11-10 11:34:50 +0800

    修改文件

--- HTERP/Code/Modules/Contact/model/CHUser.h
+++ HTERP/Code/Modules/Contact/model/CHUser.h
@@ -8,10 +8,21 @@
 
 #import "CJSONModel.h"
 
-@protocol CHUserMode
+@protocol CHUser
 @end
 
-@interface CHPerson : CJSONModel
+@interface CHUser : CJSONModel
+
+@property(nonatomic, strong)NSString<Optional> *userId;
+@property(nonatomic, strong)NSString<Optional> *name;
+@property(nonatomic, strong)NSString<Optional> *mobile;
+@property(nonatomic, strong)NSString<Optional> *companyId;
+@property(nonatomic, strong)NSString<Optional> *branchId;
+@property(nonatomic, strong)NSString<Optional> *email;
+@property(nonatomic, strong)NSString<Optional> *phone;
+@property(nonatomic, strong)NSString<Optional> *status;
+@property(nonatomic, strong)NSString<Optional> *nickname;
+@property(nonatomic, strong)NSString<Optional> *imgurl;
 
 
 
