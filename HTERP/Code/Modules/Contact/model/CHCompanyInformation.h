925ea2d4824e06122a8813c3e016cee72d782b75
macbook <wxt_zhengding@163.com>
2016-11-10 14:10:27 +0800

    添加文件

---
+++ HTERP/Code/Modules/Contact/model/CHCompanyInformation.h
@@ -0,0 +1,16 @@
+//
+//  CHCompanyInformation.h
+//  HTERP
+//
+//  Created by macbook on 10/11/2016.
+//  Copyright © 2016 Max. All rights reserved.
+//
+
+#import "CJSONModel.h"
+
+@protocol CHCompanyInformation
+@end
+
+@interface CHCompanyInformation : CJSONModel
+
+@end
---
+++ HTERP/Code/Modules/Contact/model/CHCompanyInformation.m
@@ -0,0 +1,13 @@
+//
+//  CHCompanyInformation.m
+//  HTERP
+//
+//  Created by macbook on 10/11/2016.
+//  Copyright © 2016 Max. All rights reserved.
+//
+
+#import "CHCompanyInformation.h"
+
+@implementation CHCompanyInformation
+
+@end
