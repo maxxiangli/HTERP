81b9b48570e6cc987abbef19930ef0eea6cbc213
macbook <wxt_zhengding@163.com>
2016-11-13 21:04:13 +0800

    1:添加搜素CHSearchContactViewController

---
+++ HTERP/Code/Modules/Contact/controller/CHSearchContactViewController.h
@@ -0,0 +1,13 @@
+//
+//  CHSearchContactViewController.h
+//  HTERP
+//
+//  Created by macbook on 13/11/2016.
+//  Copyright © 2016 Max. All rights reserved.
+//
+
+#import "CCustomViewController.h"
+
+@interface CHSearchContactViewController : CCustomViewController
+
+@end
