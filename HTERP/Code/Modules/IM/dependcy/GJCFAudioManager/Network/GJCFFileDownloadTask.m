b9ac71e5bb1730f0df95594a9f56ea6a7d2671f1
macbook <wxt_zhengding@163.com>
2016-11-16 20:59:03 +0800

    1:添加文件

---
+++ HTERP/Code/Modules/IM/dependcy/GJCFAudioManager/Network/GJCFFileDownloadTask.m
@@ -0,0 +1,13 @@
+//
+//  GJCFFileDownloadTask.m
+//  HTERP
+//
+//  Created by macbook on 16/11/2016.
+//  Copyright © 2016 Max. All rights reserved.
+//
+
+#import "GJCFFileDownloadTask.h"
+
+@implementation GJCFFileDownloadTask
+
+@end
---
+++ HTERP/Code/Modules/IM/dependcy/GJCFAudioManager/Network/GJCFFileUploadTask.h
@@ -0,0 +1,13 @@
+//
+//  GJCFFileUploadTask.h
+//  HTERP
+//
+//  Created by macbook on 16/11/2016.
+//  Copyright © 2016 Max. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GJCFFileUploadTask : NSObject
+
+@end
