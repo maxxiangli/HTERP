69ee4f98974b74fc8b327f523a2902b16ea7ad88
li xiang <66827030@qq.com>
2016-11-17 14:34:49 +0800

    修改：登录倒计时

---
+++ HTERP/Code/Modules/Login/IDCountDownButton.h
@@ -0,0 +1,13 @@
+//
+//  IDCountDownButton.h
+//  HTERP
+//
+//  Created by li xiang on 16/11/17.
+//  Copyright © 2016年 Max. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IDCountDownButton : UIButton
+
+@end
---
+++ HTERP/Code/Modules/Login/IDCountDownButton.m
@@ -0,0 +1,21 @@
+//
+//  IDCountDownButton.m
+//  HTERP
+//
+//  Created by li xiang on 16/11/17.
+//  Copyright © 2016年 Max. All rights reserved.
+//
+
+#import "IDCountDownButton.h"
+
+@implementation IDCountDownButton
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
